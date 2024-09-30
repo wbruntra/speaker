08affb4636d43228404e6645b4f5bd86f4a6c1ca
William Bruntrager <w.bruntrager@gmail.com>
2024-09-30 15:38:23 +0000

    Update connection, git tracking

--- db_connection.js
+++ db_connection.js
@@ -1,5 +1,7 @@
 const Knex = require('knex')
-const config = require('./knexfile.js').development
+const knexfile = require('./knexfile')
+const NODE_ENV = process.env.NODE_ENV || 'development'
+const config = knexfile[NODE_ENV]
 
 const knex = Knex(config)
 
